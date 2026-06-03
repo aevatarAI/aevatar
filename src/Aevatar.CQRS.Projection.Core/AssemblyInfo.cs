7468069a6b6b396b0315bb25338997b4f58c51d7
loning <aloning@gmail.com>
2026-06-03 17:08:43 +0800

    refactor(#1725): 移动 CommittedStateEventEnvelope 到 Projection.Core.Abstractions 斩断 Channel.Abstractions→Projection.Core 反向依赖 (#1739) (#1741)
    * 斩断 Channel.Abstractions 到 Projection.Core 的反向依赖 #1725
    
    * 修复 #1725 #1739 审查拒绝项

--- src/Aevatar.CQRS.Projection.Core/AssemblyInfo.cs
+++ src/Aevatar.CQRS.Projection.Core/AssemblyInfo.cs
@@ -1,3 +1,5 @@
 using System.Runtime.CompilerServices;
+using Aevatar.CQRS.Projection.Core.Orchestration;
 
 [assembly: InternalsVisibleTo("Aevatar.CQRS.Projection.Core.Tests")]
+[assembly: TypeForwardedTo(typeof(CommittedStateEventEnvelope))]
